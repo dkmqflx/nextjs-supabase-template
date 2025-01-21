9760eb4709f9701ba25cf6a5d82d53d58e301dff
dkmqflx <dkmqflx@gmail.com>
2025-01-21 21:18:13 +0900

    refactor: change query provider name

--- src/app/providers/QueryProvider.tsx
+++ src/app/providers/QueryProvider.tsx
@@ -5,7 +5,11 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import type * as React from "react";
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export default function QueryProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const queryClient = getQueryClient();
 
   return (
