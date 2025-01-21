692e17c3c26bb18b667f07f5dda52a1d8cc59472
dkmqflx <dkmqflx@gmail.com>
2025-01-21 20:52:24 +0900

    refactor: change shadcn component install path

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/shared/ui/button";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
+      <Button>Shadcn Button</Button>
       <Link href="/prefetch">prefetch</Link>
     </div>
   );
