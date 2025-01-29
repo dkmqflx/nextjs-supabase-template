223a83f6dcbc46ef3944a4e7ddffd187eed9bb50
dkmqflx <dkmqflx@gmail.com>
2025-01-29 17:58:19 +0900

    chore: add query devtools

--- src/app/providers/QueryProvider.tsx
+++ src/app/providers/QueryProvider.tsx
@@ -2,9 +2,15 @@
 
 import { getQueryClient } from '@/shared/lib/get-query-client';
 import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const queryClient = getQueryClient();
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-right" />
+    </QueryClientProvider>
+  );
 }
