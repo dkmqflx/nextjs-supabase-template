dc3bf29fc3fc9073ee7d806077a7a992ef254c44
dkmqflx <dkmqflx@gmail.com>
2025-02-03 17:48:50 +0900

    feat: add login form

---
+++ src/features/login/index.ts
@@ -0,0 +1,2 @@
+export { Kakao } from './ui/Kakao';
+export { default as SignIn } from './ui/SignInForm';
---
+++ src/features/login/ui/SignInForm.tsx
@@ -0,0 +1,115 @@
+'use client';
+
+import { useState } from 'react';
+
+import { Button } from '@/shared/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/shared/ui/card';
+import { Input } from '@/shared/ui/input';
+import { Label } from '@/shared/ui/label';
+import { Separator } from '@/shared/ui/separator';
+
+const SignInForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Handle login logic here
+  };
+
+  const handleSocialLogin = () => {
+    // Handle social login logic here
+    // console.log(`Attempting to login with ${provider}`);
+  };
+
+  return (
+    <main className="flex h-screen w-full items-center justify-center">
+      <Card className="w-[450px]">
+        <CardHeader>
+          <CardTitle>Login</CardTitle>
+          <CardDescription>Enter your credentials to access your account.</CardDescription>
+        </CardHeader>
+
+        <form onSubmit={handleSubmit}>
+          <CardContent className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="you@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+
+            <Button type="submit" className="w-full">
+              Sign In
+            </Button>
+          </CardContent>
+        </form>
+
+        <CardContent>
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <Separator />
+            </div>
+
+            <div className="relative flex justify-center text-xs uppercase">
+              <span className="bg-background px-2 text-muted-foreground">Or continue with</span>
+            </div>
+          </div>
+        </CardContent>
+
+        <CardFooter className="flex justify-between">
+          <Button variant="outline" className="w-[48%]" onClick={() => handleSocialLogin()}>
+            <svg
+              className="mr-2 h-4 w-4"
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="google"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 488 512"
+            >
+              <path
+                fill="currentColor"
+                d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+              ></path>
+            </svg>
+            Google
+          </Button>
+
+          <Button variant="outline" className="w-[48%]" onClick={() => handleSocialLogin()}>
+            <svg
+              className="mr-2 h-4 w-4"
+              aria-hidden="true"
+              focusable="false"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path d="M12 2C6.48 2 2 5.94 2 10.88c0 3.07 1.81 5.8 4.59 7.5l-1.2 3.49a.5.5 0 0 0 .71.61l3.88-2.2c.63.09 1.28.14 1.93.14 5.52 0 10-3.94 10-8.88S17.52 2 12 2z" />
+            </svg>
+            Kakao
+          </Button>
+        </CardFooter>
+      </Card>
+    </main>
+  );
+};
+
+export default SignInForm;
