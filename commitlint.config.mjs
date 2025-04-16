2e440ee2ed2c1a696b1471ee5d46c5f9ecf31ea8
dkmqflx <dkmqflx@gmail.com>
2025-04-16 17:16:47 +0900

    ci: add GitHub Actions workflow for test and build

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -3,7 +3,7 @@ const Configuration = {
   rules: {
     'header-min-length': [2, 'always', 10],
     'subject-full-stop': [2, 'never', '.'],
-    'type-enum': [2, 'always', ['feat', 'fix', 'style', 'chore', 'refactor', 'docs', 'test']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'style', 'chore', 'refactor', 'docs', 'test', 'ci']],
     'body-leading-blank': [0, 'never'],
   },
 };
