bcf2991a8a4c0054f307a8708c0e5132e91ebff5
dkmqflx <dkmqflx@gmail.com>
2025-04-18 15:02:45 +0900

    refactor: remove zod validation in api request function

--- src/shared/api/apiClient.ts
+++ src/shared/api/apiClient.ts
@@ -1,37 +1,23 @@
-import { ZodSchema, z } from 'zod';
-
 import { BASE_URL } from '../constants/baseUrl';
 import type { BaseResponse } from './types';
 
-class ApiClient {
+export class ApiClient {
   private baseUrl: string;
 
   constructor(url: string) {
     this.baseUrl = url;
   }
 
-  private async handleResponse<T>(response: Response, schema?: ZodSchema<T>): Promise<BaseResponse<T>> {
+  private async handleResponse<T>(response: Response): Promise<BaseResponse<T>> {
     if (!response.ok) {
       const result = await response.json();
-
       throw new Error(`${result?.message ?? `HTTP error. Status Code: ${response.status}`}`);
     }
 
     try {
       const result = await response.json();
-
-      // Validate response using Zod
-      if (schema) {
-        schema.parse(result.data);
-      }
-
       return result;
     } catch (error) {
-      if (error instanceof z.ZodError) {
-        console.warn('Validation error:', error.errors);
-        throw new Error('API response validation failed');
-      }
-
       console.warn('Error parsing JSON response:', error);
       throw new Error('Error parsing JSON response');
     }
@@ -52,7 +38,6 @@ class ApiClient {
   private async request<T>(
     method: string,
     endpoint: string,
-    schema?: ZodSchema<T>,
     options?: RequestInit,
     body?: Record<string, unknown>,
     queryParams?: Record<string, string | number | boolean>,
@@ -69,38 +54,27 @@ class ApiClient {
       ...options,
     });
 
-    return this.handleResponse<T>(response, schema);
+    return this.handleResponse<T>(response);
   }
 
-  public get<T>(
-    endpoint: string,
-    schema?: ZodSchema<T>,
-    queryParams?: Record<string, string | number | boolean>,
-    options?: RequestInit,
-  ) {
-    return this.request<T>('GET', endpoint, schema, options, undefined, queryParams);
+  public get<T>(endpoint: string, queryParams?: Record<string, string | number | boolean>, options?: RequestInit) {
+    return this.request<T>('GET', endpoint, options, undefined, queryParams);
   }
 
   public post<T, TData extends Record<string, unknown> | undefined>(
     endpoint: string,
     body: TData,
-    schema?: ZodSchema<T>,
     options?: RequestInit,
   ) {
-    return this.request<T>('POST', endpoint, schema, options, body);
+    return this.request<T>('POST', endpoint, options, body);
   }
 
-  public put<T, TData extends Record<string, unknown>>(
-    endpoint: string,
-    body: TData,
-    schema?: ZodSchema<T>,
-    options?: RequestInit,
-  ) {
-    return this.request<T>('PUT', endpoint, schema, options, body);
+  public put<T, TData extends Record<string, unknown>>(endpoint: string, body: TData, options?: RequestInit) {
+    return this.request<T>('PUT', endpoint, options, body);
   }
 
   public delete<T>(endpoint: string, options?: RequestInit) {
-    return this.request<T>('DELETE', endpoint, undefined, options);
+    return this.request<T>('DELETE', endpoint, options);
   }
 }
 
